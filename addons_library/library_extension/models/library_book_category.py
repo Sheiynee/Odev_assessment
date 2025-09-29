784b39f4f50a931fccfb282d123bf9c465230e40
Sheiynee <137514304+Sheiynee@users.noreply.github.com>
2025-09-29 15:06:36 +0000

    add another validation for case sensitivity

--- addons_library/library_extension/models/library_book_category.py
+++ addons_library/library_extension/models/library_book_category.py
@@ -10,3 +10,10 @@ class LibraryCategory(models.Model):
     _sql_constraints = [
         ('unique_category_name', 'UNIQUE(name)', 'The Category name already exists!')
     ]
+
+     @api.constrains('name')
+    def _check_unique_name(self):
+        for rec in self:
+            existing = self.search([('id', '!=', rec.id), ('name', 'ilike', rec.name)], limit=1)
+            if existing:
+                raise ValidationError("The Category name already exists!")
