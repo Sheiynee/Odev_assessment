ffd3bde486e43c255dc6dc090308dc480395c360
Sheiynee <137514304+Sheiynee@users.noreply.github.com>
2025-09-29 14:12:05 +0000

    try a different approach

--- addons_library/library_extension/models/library_book_category.py
+++ addons_library/library_extension/models/library_book_category.py
@@ -2,14 +2,14 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class LibraryCategory(models.Model):
-    _name = "library.book.category"
+    __name__ = "library.book.category"
     _description = "Library Book Category"
     _order = "name asc"
     
     name = fields.Char('Category Name', required=True, compute='_compute_unique_name', store=True)
 
-    @api.depends('name')
-    def _compute_unique_name(self):
+    @api.constrains('name')
+    def _check_unique_name(self):
         for record in self:
-            if record.name == record.name:
-                raise UserError("The Category name is already been used")
+            if self.search_count([('name', '=', record.name)]) > 1:
+                raise ValidationError("Category name must be unique.")
