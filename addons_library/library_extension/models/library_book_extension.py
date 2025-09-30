5b6d9e3085b2850fbf2ec822cce0c23ec41144e6
Sheiynee <137514304+Sheiynee@users.noreply.github.com>
2025-09-30 13:00:32 +0000

    fix the library book extension multiple error

--- addons_library/library_extension/models/library_book_extension.py
+++ addons_library/library_extension/models/library_book_extension.py
@@ -4,10 +4,4 @@ class LibraryExtension(models.Model):
     _inherit = "library.book"
 
     author_id = fields.Many2one('res.partner', string='Author', required=True)
-from odoo import models, fields, api
-
-class LibraryExtension(models.Model):
-    _inherit = "library.book"
-
-    author_id = fields.Many2one('res.partner', string='Author', required=True)
     category_id = fields.Many2many('library.book.category', string='Category', widget='many2many_tags')
